5fd8737bfcf938e2cc79921fa39770b921590217
Justin Chang <justin@aptoslabs.com>
2023-06-29 10:39:24 -0700

    temp docker

--- ecosystem/nft-metadata-crawler/src/lib.rs
+++ ecosystem/nft-metadata-crawler/src/lib.rs
@@ -10,7 +10,7 @@ use diesel::prelude::*;
 use std::env;
 
 pub fn establish_connection() -> PgConnection {
-    let database_url = env::var("NFT_METADATA_CRAWLER_URL").expect("NFT_METADATA_CRAWLER_URL must be set");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
